e6efe72e41f567bc8587e95fcc50563fcb184f88
Emergenzy <emergenzy18@gmail.com>
2026-04-22 00:05:48 +0700

    add deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   devIndicators: false,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
+
 
 export default nextConfig;
